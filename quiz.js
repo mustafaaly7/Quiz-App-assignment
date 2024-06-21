a469a40bec26a23307a8f50c3ed45fd27ff92c5c
Mustafa Ali <mustafaaaly27@gmail.com>
2024-06-21 23:37:28 +0500

    completed just result page remainig

--- quiz.js
+++ quiz.js
@@ -1,104 +1,119 @@
 // for quiz and its answer we need to create array of objects 
 
-var questions =[
+var questions = [
     {
-        id : 1,
-        question :"What does HTML stand for?",
+        id: 1,
+        question: "What does HTML stand for?",
         answer: {
-            a:"Home tool Markup language",
-            b:"Hyper Text markup language",
-            c:"Hyperlinks and text markup language",
-            d:"none of the above"
+            a: "Home tool Markup language",
+            b: "Hyper Text markup language",
+            c: "Hyperlinks and text markup language",
+            d: "none of the above"
 
         },
-        result:"Hyper Text markup language"
+        result: "Hyper Text markup language"
     },
     {
-        id : 2,
-        question :"Who is making the Web standards?",
+        id: 2,
+        question: "Who is making the Web standards?",
         answer: {
-            a:"The world wide web consortium",
-            b:"Google",
-            c:"microsoft",
-            d:"mozilla"
+            a: "The world wide web consortium",
+            b: "Google",
+            c: "microsoft",
+            d: "mozilla"
 
         },
-        result:"The world wide web consortium"
+        result: "The world wide web consortium"
     },
     {
-        id : 3,
-        question :"What is the difference between XML and HTML?",
+        id: 3,
+        question: "What is the difference between XML and HTML?",
         answer: {
-            a:"HTML is used for exchanging data, XML is not.",
-            b:"XML is used for exchanging data, HTML is not",
-            c:"HTML can have user defined tags, XML cannot",
-            d:"None"
+            a: "HTML is used for exchanging data, XML is not.",
+            b: "XML is used for exchanging data, HTML is not",
+            c: "HTML can have user defined tags, XML cannot",
+            d: "None"
 
         },
-        result:"XML is used for exchanging data, HTML is not"
+        result: "XML is used for exchanging data, HTML is not"
     },
     {
-        id : 4,
-        question :"Opening Tag of HTML Tag is called as ________.",
+        id: 4,
+        question: "Opening Tag of HTML Tag is called as ________.",
         answer: {
-            a:"Closed Tag",
-            b:"Starting Tag",
-            c:"Forward Tag",
-            d:"Enging Tag"
+            a: "Closed Tag",
+            b: "Starting Tag",
+            c: "Forward Tag",
+            d: "Enging Tag"
 
         },
-        result:"Starting Tag"
+        result: "Starting Tag"
     },
     {
-        id : 5,
-        question : "HTML program is saved using _________ extension.",
+        id: 5,
+        question: "HTML program is saved using _________ extension.",
         answer: {
-            a:".htl",
-            b:'.htlm',
-            c:".html",
-            d:"All of them"
+            a: ".htl",
+            b: '.htlm',
+            c: ".html",
+            d: "All of them"
 
         },
-        result:".html"
+        result: ".html"
     },
     {
-        id : 6,
-        question :"DOM stands for",
+        id: 6,
+        question: "DOM stands for",
         answer: {
-            a:"Document object model",
-            b:"Data object model",
-            c:"Document Oriented model",
-            d:"Data oriented model"
+            a: "Document object model",
+            b: "Data object model",
+            c: "Document Oriented model",
+            d: "Data oriented model"
 
         },
-        result:"Document object model"
+        result: "Document object model"
     },
     {
-        id : 7,
-        question :"ASCII stands for______",
+        id: 7,
+        question: "ASCII stands for______",
         answer: {
-            a:"American Standard Code for Informal Interchange",
-            b:"American Standard Code for Interchanging Information",
-            c:"American Standard Code for Information Interchange",
-            d:"none of them"
+            a: "American Standard Code for Informal Interchange",
+            b: "American Standard Code for Interchanging Information",
+            c: "American Standard Code for Information Interchange",
+            d: "none of them"
 
         },
-        result:"American Standard Code for Information Interchange"
+        result: "American Standard Code for Information Interchange"
     },
     {
-        id : 8,
-        question :"Which of the following are attributes of Font Tag ?",
+        id: 8,
+        question: "Which of the following are attributes of Font Tag ?",
         answer: {
-            a:"Face",
-            b:"Size ",
-            c:"Color",
-            d:"all of them"
+            a: "Face",
+            b: "Size ",
+            c: "Color",
+            d: "all of them"
 
         },
-        result:"all of them"
+        result: "all of them"
     }
 ]
+var min =document.getElementById("min")
+var sec =document.getElementById("sec")
+var interval ;
+interval = setInterval(startTimer,1000) 
+    function startTimer(){
+        sec.innerHTML --
+    if(sec.innerHTML == 0){
+        min.innerHTML -- 
+        sec.innerHTML = 59
+    }
 
+    if(min.innerHTML == 0 ){
+        alert("times UP!!")
+    }
+    }
+var main = document.getElementById("main")
 
 var name = localStorage.getItem("name")
 var email = localStorage.getItem("email")
@@ -109,8 +124,25 @@ var userEmail = document.getElementById("userEmail")
 userName.innerHTML = name;
 userEmail.innerHTML = email
 
+userName.style.color = "black"
+userEmail.style.color = "black"
+userName.style.fontSize = "1.2em"
+userEmail.style.fontSize = "1.2em"
+
+
+
+
+
+
+
+
+
+
+
+
+
 var indexcount = 0
-var htmlQ = document.getElementById("htmlQ") 
+var htmlQ = document.getElementById("htmlQ")
 var options = document.getElementById("options")
 var startCounter = document.getElementById("startCounter")
 var endCounter = document.getElementById("endCounter")
@@ -118,15 +150,27 @@ endCounter.innerHTML = questions.length
 
 var nextBtn = document.getElementById("nextBtn")
 
-function question(){
-// console.log(questions[indexcount]);
-htmlQ.innerHTML = questions[indexcount].question
-options.innerHTML = ''
+function question() {
 
-for(var key in questions[indexcount].answer){
-    var option = questions[indexcount].answer[key] 
-    options.innerHTML += `<li onclick = "checkAnswer(this)">${option}`
-}
+    
+    // console.log(questions[indexcount]);
+
+
+
+
+
+
+
+
+
+
+    htmlQ.innerHTML = questions[indexcount].question
+    options.innerHTML = ''
+
+    for (var key in questions[indexcount].answer) {
+        var option = questions[indexcount].answer[key]
+        options.innerHTML += `<li onclick = "checkAnswer(this)">${option}`
+    }
 
 
 }
@@ -134,16 +178,16 @@ for(var key in questions[indexcount].answer){
 var parentButton = document.getElementById("parentButton")
 
 
-function nextQuestion(btn){
-    if(indexcount < questions.length-1){
+function nextQuestion(btn) {
+    if (indexcount < questions.length - 1) {
         indexcount++
-        startCounter.innerHTML ++
-        
+        startCounter.innerHTML++
+
     }
-    if(indexcount == questions.length-1){
+    if (indexcount == questions.length - 1) {
         // btn.className = "hidden"
-    nextBtn.innerHTML = "Check Result"
-    nextBtn.setAttribute("onclick" , "showResult()")
+        nextBtn.innerHTML = "Check Result"
+        nextBtn.setAttribute("onclick", "showResult()")
 
         // var pBtn = document.createElement("button")
         // pBtn.innerHTML = "Previous Question"
@@ -169,27 +213,56 @@ function nextQuestion(btn){
 var correctAnswer = 0;
 var wrongAnswer = 0;
 
-function checkAnswer(ele){
+var correct = document.getElementById("correct")
+var wrong = document.getElementById("wrong")
+correct.innerHTML = correctAnswer
+wrong.innerHTML = wrongAnswer
+
+function checkAnswer(ele) {
+    var liOptions = options.getElementsByTagName("li")
+    var check = ele.innerHTML == questions[indexcount].result
+
     // console.log(ele.innerHTML);
-    if(ele.innerHTML == questions[indexcount].result ){
+    // if(ele.innerHTML == questions[indexcount].result ){
+    //     ele.style.backgroundColor = "green"
+    //     correctAnswer ++
+    //     ele.style.cursor = "no-drop"
+
+    // }else{
+    //     ele.style.backgroundColor = "red"
+    //     wrongAnswer ++
+    //     ele.style.cursor = "no-drop"
+
+    // }
+    if (check) {
+        // console.log("hello mf");
+        correctAnswer++;
         ele.style.backgroundColor = "green"
-        nextBtn.className = "show"
-        correctAnswer ++
-        ele.style.cursor = "no-drop"
-
-    }else{
-        ele.style.backgroundColor = "red"
-        nextBtn.className = "show"
-        wrongAnswer ++
-        ele.style.cursor = "no-drop"
-
+    } else {
+        // console.log("fuckk off");
+        ele.style.backgroundColor = "darkred"
+        wrongAnswer++
+        for (var li of liOptions) {
+            if (li.innerHTML === questions[indexcount].result) {
+                li.style.backgroundColor = "green"
+            }
+        }
     }
+
+    for (var li of liOptions) {
+        // console.log("li")
+        li.style.pointerEvents = "none"
+        li.style.cursor = "no-drop !important"
+    }
+    nextBtn.className = "show"
 }
 
 
-
-function showResult(){
-    console.log(correctAnswer , wrongAnswer);
+var Result = document.getElementById("Result")
+function showResult() {
+    // console.log(correctAnswer, wrongAnswer);
+    // main.className = "hidden"
+    // Result.className = "show"
 }
 
 
